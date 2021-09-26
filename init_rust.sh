899507a2e1d785f8d40841734671bf3e65275649
7p7 <z2z4z678@163.com>
2021-09-27 00:24:49 +0800

    add ustc mirror for rustup

--- init_rust.sh
+++ init_rust.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 echo 'export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup' >> ~/.bash_profile
+echo '# export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static' >> ~/.bash_profile
+
 bash ~/fun/setup/rust.sh
 
 source ~/.bash_profile
